9ef50a9be11337d8321c65676b0de900e00ab61b
Your Name <you@example.com>
2024-12-02 19:02:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import React from 'react';
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
